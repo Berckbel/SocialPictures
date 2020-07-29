5222c329323bfbe0dc8c494be7ed65debb19f220
Berckbel <berckbel@gmail.com>
2020-07-28 22:05:13 -0500

    Routes with express

---
+++ src/controllers/image.js
@@ -0,0 +1,23 @@
+const ctrl = {};
+
+ctrl.index = (req, res)=>{
+    res.send('Index page');
+};
+
+ctrl.create = (req, res)=>{
+    res.send('Create page');
+};
+
+ctrl.like = (req, res)=>{
+    res.send('Like page');
+};
+
+ctrl.comment = (req, res)=>{
+    res.send('Comment page');
+};
+
+ctrl.remove = (req, res)=>{
+    res.send('Remove page');
+};
+
+module.exports = ctrl;
