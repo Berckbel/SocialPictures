734b7395eb9d02252f86fae15a0c54b508f74bb7
Berckbel <berckbel@gmail.com>
2020-07-29 19:10:33 -0500

    Savign images in mongo

--- src/controllers/image.js
+++ src/controllers/image.js
@@ -2,35 +2,56 @@ const path = require('path');
 const { randomNumber } = require('../helpers/libs');
 const fs = require('fs-extra');
 
+const { Image } = require('../models');
+
 const ctrl = {};
 
-ctrl.index = (req, res)=>{
+ctrl.index = (req, res) => {
     res.send('Index page');
 };
 
-ctrl.create = async (req, res)=>{
-    const imgUrl = randomNumber();
-    console.log(imgUrl);
-    const imageTempPath = req.file.path;
-    const ext = path.extname(req.file.originalname).toLowerCase();
-    const targetPath = path.resolve(`src/public/upload/${imgUrl}${ext}`);
+ctrl.create = (req, res) => {
 
-    if (ext === '.png' || ext === '.jpg' || ext === 'jpeg' || ext === '.gif') {
-        await fs.rename(imageTempPath, targetPath);
-    }
+    const saveImage = async () => {
+        const imgUrl = randomNumber();
+        const images = await Image.find({ filename: imgUrl });
+        if (images.length > 0) {
+            saveImage();
+        } else {
+            console.log(imgUrl);
+            const imageTempPath = req.file.path;
+            const ext = path.extname(req.file.originalname).toLowerCase();
+            const targetPath = path.resolve(`src/public/upload/${imgUrl}${ext}`);
 
-    res.send('Works');
+            if (ext === '.png' || ext === '.jpg' || ext === 'jpeg' || ext === '.gif') {
+                await fs.rename(imageTempPath, targetPath);
+                const newImg = new Image({
+                    title: req.body.title,
+                    filename: imgUrl + ext,
+                    description: req.body.description,
+                });
+                const imageSaved = await newImg.save();
+                res.send('works!!');
+            } else {
+                await fs.unlink(imageTempPath);
+                res.status(500).json({ error: 'Only Images are allowed' });
+            }
+        }
+    };
+
+    saveImage();
+
 };
 
-ctrl.like = (req, res)=>{
+ctrl.like = (req, res) => {
     res.send('Like page');
 };
 
-ctrl.comment = (req, res)=>{
+ctrl.comment = (req, res) => {
     res.send('Comment page');
 };
 
-ctrl.remove = (req, res)=>{
+ctrl.remove = (req, res) => {
     res.send('Remove page');
 };
 
