765eebdeb5c33932c127f13ca54db380fe886b9d
Berckbel <berckbel@gmail.com>
2020-07-30 23:18:19 -0500

    Likes

---
+++ src/public/js/scripts.js
@@ -0,0 +1,9 @@
+$('#btn-like').click(function(e){
+    e.preventDefault();
+    let imageId = $(this).data('id');
+    
+    $.post('/images/' + imageId +'/like').done(data =>{
+        console.log(data);
+        $('.likes-count').text(data.likes);
+    });
+});
